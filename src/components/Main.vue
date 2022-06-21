fa919c209e9f38fde4aa5447350f97d0ed4e0ecf
DelmerRo <jindrg@gmail.com>
2022-06-21 20:37:56 -0300

    commit De Prueba

---
+++ src/components/Main.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="">
+    <h1>src-components-main Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'src-components-main',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .src-components-main {
+
+  }
+</style>
