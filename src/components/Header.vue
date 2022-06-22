d8f2eb483e8f63fd942f7f9c59746892f58e885d
Mario Ferrario <marioferrarioas@gmail.com>
2022-06-22 08:49:36 -0300

    Creacion del Header

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <section class="src-components-header">
+    <img src="./assets/logo.png" alt="Logo de Veterinaria">
+  </section>
+
+</template>
+
+<script>
+
+  export default  {
+    name: 'src-components-header',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-header {
+
+  }
+</style>
