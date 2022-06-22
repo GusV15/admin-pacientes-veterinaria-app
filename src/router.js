d231b3fb0261d040a5fdd5bfedc5491f425c636b
DelmerRo <jindrg@gmail.com>
2022-06-21 22:55:54 -0300

    Eliminando main

--- src/router.js
+++ src/router.js
@@ -1,17 +1,13 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-import Main from './components/main.vue'
-/* import AddAppointment from './components/addappointment.vue'
-import HistoryAppoiment from './components/historyappoiment.vue' */
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
   mode: "history",
   routes: [
-   console.log(Main),
-   /*  { path: '/', redirect: '/main' }, */
-         { path: '/form', component: Main },
-        /*{ path: '/requests', component: HistoryAppoiment }, */
+    /* { path: '/', redirect: '/form' },
+        { path: '/form', component: Form },
+        { path: '/requests', component: Requests }, */
   ],
 });
