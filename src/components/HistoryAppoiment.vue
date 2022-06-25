87b823dac98d2996df8fea2365c76e73420d54c9
DelmerRo <jindrg@gmail.com>
2022-06-25 13:04:44 -0300

    Se agrega global Filters

--- src/components/HistoryAppoiment.vue
+++ src/components/HistoryAppoiment.vue
@@ -2,9 +2,8 @@
 
   <section class="src-components-history-appoiment">
      <div class="jumbotron">
-    <h2>History Appointment</h2>
+    <h2>{{ 'History Appointment' | pasarAMayuscula}}</h2>
       <hr>
-      <br>
    
    <form  novalidate @submit.prevent="enviar()">
 
@@ -24,15 +23,17 @@
           <div v-if="!formData.nombre">Campo requerido</div>
           <div v-else-if="formData.nombre.length<3">Este campo requiere al menos 3 caracteres</div>
         </div>
+
       </div>
+      
       </form>
         <br>
       <div  class="media alert alert-info" v-for="(cita,index) in mostrarAtendidos(formData.nombre)  "   :key="index" >    
-  <img v-if="cita.atendido"  :src="cita.foto" width="150" :alt="cita.nombre" :style="{'border-radius': '10px'}"> 
+               <img v-if="cita.atendido"  :src="cita.foto" width="150" :alt="cita.nombre" :style="{'border-radius': '10px'}"> 
          <div  class="media-body ml-4">
-              <h5 class="mt-0">
-                <u>dating history {{ index + 1 }}  - create: {{ convertirFyh(cita.fecha_hora_atencion) }}</u>
-              </h5>
+              <h6 class="mt-0">
+               <b> {{ 'dating history' | pasarAMayuscula}} {{ index + 1 }}  - {{ 'create' | pasarAMayuscula}} {{ convertirFyh(cita.fecha_hora_atencion) }}</b>
+              </h6>
               <p>Pet: <b>{{ cita.nombre }}</b></p>
               <p >Human Name: <b>{{ cita.nombre_duenio }}</b></p>
               <p>Date and time of attention: <b>{{ cita.fecha_hora_atencion }}</b></p>
@@ -42,18 +43,12 @@
               <!-- <button class="btn btn-danger mt-3" @click="deleteHistorial(cita.id)">DELETE</button> -->
           </div>
           </div>
-         <h6 class="alert alert-secondary">
-         <div v-if="!calcularHistorialPorNombre.existeEnGeneral " > No hay Registro</div>
-         <span v-if="this.formData.nombre ==null || this.formData.nombre ==''">Total de registros: {{ calcularHistorialPorNombre.totalEnGeneral }}</span>
-        <!--   <span v-else-if="this.formData.nombre !=null">All records {{ calcularHistorialPorNombre.cantidadPorNombre }} </span> -->
-                  <span v-else-if="this.formData.nombre !=null">
+     
+         <span class="alert alert-danger" v-if="!calcularHistorialPorNombre.existeEnGeneral " > No hay Registro</span>
+         <span :class="getClass(!estado)" v-else-if="this.formData.nombre ==null || this.formData.nombre ==''">Total de registros: {{ calcularHistorialPorNombre.totalEnGeneral }}</span>
+                  <span :class="getClass(estado)" v-else-if="this.formData.nombre !=null">
                       Se encontro {{ calcularHistorialPorNombre.cantidadPorNombre }} de {{ calcularHistorialPorNombre.totalEnGeneral }} registros
-                   <!--   {{calcularHistorialPorNombre.todos}}
-                     {{this.formData.nombre!=''}} -->
                   </span>
-                  
-                 
-      </h6>
       </div>
   </section>
 
@@ -72,6 +67,7 @@ this.getCitas()
 url:'https://62861febf0e8f0bb7c10b9cd.mockapi.io/citas',
 citas:[],
 nombreMinLength:2,
+estado:false,
 formData : this.getInicialData(),
 formDirty : this.getInicialData()
       }
@@ -80,9 +76,7 @@ methods: {
       convertirFyh(fyh) {
         return new Date(fyh).toLocaleString()
       },
-      /* ---------------------------------- */
-      /*           API REST : GET           */
-      /* ---------------------------------- */
+      
       async getCitas() {
         try {
           let { data: citas } = await this.axios(this.url)
@@ -110,7 +104,11 @@ methods: {
       console.log({...this.formData})
       this.formData = this.getInicialData()
       this.formDirty = this.getInicialData()
-    }
+    },
+
+    getClass(estado) {
+          return [{ 'alert alert-secondary':estado,'alert alert-success':!estado }, 'text-black', 'p-2', 'rounded']            
+      }
 
     },
 computed: {
@@ -118,11 +116,6 @@ computed: {
   let cant = this.citas.filter(cita => cita.atendido && cita.nombre==this.formData.nombre).length
           let total = this.citas.filter(cita => cita.atendido).length
       return{
-      /*   ninguno : totalAtendidos == 0,
-        totalAtendidos:totalAtendidos,
-        totalPorNombre:totalPorNombre,
-        nombre:nombre */
-
               ningunoPorNombre : cant == 0,
               existeEnGeneral: total > 0,
               existePorNombre: cant > 0,
