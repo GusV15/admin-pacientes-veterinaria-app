a582e6b69e6cc3d1096f78431f3d5450636870cb
DelmerRo <jindrg@gmail.com>
2022-06-26 23:47:27 -0300

    craeando componente Patients

---
+++ src/components/Patients.vue
@@ -0,0 +1,40 @@
+<template >
+
+  <section class="src-components-patients">
+     <div class="jumbotron">
+      <h2>{{ "Patients" | pasarAMayuscula }}</h2>
+      <hr />
+      </div>
+  </section>
+
+</template>
+
+<script >
+
+  export default  {
+    name: 'src-components-patients',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-patients {
+
+  }
+</style>
